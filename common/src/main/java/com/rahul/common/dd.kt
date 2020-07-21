230b7f792879fe8bd07b85b320dc9c02e6cc194c
Rahul Giradkar <rahulgiradkar00@gmail.com>
2020-07-21 12:23:23 +0530

    Initial

--- common/src/main/java/com/rahul/common/dd.kt
+++ common/src/main/java/com/rahul/common/dd.kt
@@ -2,7 +2,8 @@ package com.rahul.common
 
 class dd {
     fun demo(vararg a: Int) {
-        var calculation=Calculation()
-        var sum=calculation.addition(1,1,2);
+
+        var calculation = Calculation()
+        var sum = calculation.addition(1, 1, 2)
     }
 }
